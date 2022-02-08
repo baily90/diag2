9f45e1a17b71f5ceec4af1f7641fd5990803063b
zhangyanlong <yanlongzhang1990@gmail.com>
2022-02-08 14:49:00 +0800

    feat(*): test git-msg

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react/jsx-filename-extension': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'react/react-in-jsx-scope': 0,
+    'import/no-extraneous-dependencies': 0,
+    'global-require': 0,
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent } from "react";
+
+interface AppProps {
+  
+}
+ 
+const App: FunctionComponent<AppProps> = () => {
+  return (
+    <>App</>
+  );
+}
+ 
+export default App;
